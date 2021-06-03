440042bcf104d9a2235c19e54a9e4877e1c3cbaf
Atharva-Gundawar <atharva.n.gundawar@gmail.com>
2021-06-03 22:37:40 +0530

    Fix : naming

---
+++ src/FaceRecogOnFeed.py
@@ -0,0 +1,94 @@
+import face_recognition
+import os
+import cv2
+
+
+KNOWN_FACES_DIR = 'known_faces'
+TOLERANCE = 0.6
+FRAME_THICKNESS = 3
+FONT_THICKNESS = 2
+MODEL = 'hog'  # default: 'hog', other one can be 'cnn' - CUDA accelerated (if available) deep-learning pretrained model
+
+
+# Returns (R, G, B) from name
+def name_to_color(name):
+    # Take 3 first letters, tolower()
+    # lowercased character ord() value rage is 97 to 122, substract 97, multiply by 8
+    color = [(ord(c.lower())-97)*8 for c in name[:3]]
+    return color
+
+
+print('Loading known faces...')
+known_faces = []
+known_names = []
+
+# We oranize known faces as subfolders of KNOWN_FACES_DIR
+# Each subfolder's name becomes our label (name)
+for name in os.listdir(KNOWN_FACES_DIR):
+
+    # Next we load every file of faces of known person
+    for filename in os.listdir(f'{KNOWN_FACES_DIR}/{name}'):
+
+        # Load an image
+        image = face_recognition.load_image_file(f'{KNOWN_FACES_DIR}/{name}/{filename}')
+
+        # Get 128-dimension face encoding
+        # Always returns a list of found faces, for this purpose we take first face only (assuming one face per image as you can't be twice on one image)
+        encoding = face_recognition.face_encodings(image)[0]
+
+        # Append encodings and name
+        known_faces.append(encoding)
+        known_names.append(name)
+
+
+cap =cv2.VideoCapture(0)
+
+while True:
+	# grab the frame from the threaded video stream and resize it
+	# to have a maximum width of 600 pixels
+    _, frame = cap.read()
+    locations = face_recognition.face_locations(frame, model=MODEL)
+    encodings = face_recognition.face_encodings(frame, locations)
+    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+
+    for face_encoding, face_location in zip(encodings, locations):
+        results = face_recognition.compare_faces(known_faces, face_encoding, TOLERANCE)
+        match = None
+        if True in results:  # If at least one is true, get a name of first of found labels
+            match = known_names[results.index(True)]
+            print(f' - {match} from {results}')
+
+            # Each location contains positions in order: top, right, bottom, left
+            top_left = (face_location[3], face_location[0])
+            bottom_right = (face_location[1], face_location[2])
+
+            # Get color by name using our fancy function
+            color = name_to_color(match)
+
+            # Paint frame
+            cv2.rectangle(frame, top_left, bottom_right, color, FRAME_THICKNESS)
+
+            # Now we need smaller, filled grame below for a name
+            # This time we use bottom in both corners - to start from bottom and move 50 pixels down
+            top_left = (face_location[3], face_location[2])
+            bottom_right = (face_location[1], face_location[2] + 22)
+
+            # Paint frame
+            cv2.rectangle(frame, top_left, bottom_right, color, cv2.FILLED)
+
+            # Wite a name
+            cv2.putText(frame, match, (face_location[3] + 10, face_location[2] + 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (200, 200, 200), FONT_THICKNESS)
+        
+
+    # Display feed
+    cv2.imshow("output", frame)
+    cv2.destroyAllWindows()
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+  
+# After the loop release the cap object
+cap.release()
+# Destroy all the windows
+cv2.destroyAllWindows()
+
