5db2a61e6aff5224b5fef61cd8c82b4e1e4f2c92
Atharva-Gundawar <atharva.n.gundawar@gmail.com>
2021-06-03 22:33:00 +0530

    Fix : naming and requirments

--- train.py
+++ train.py
@@ -6,7 +6,7 @@ import matplotlib
 matplotlib.use("Agg")
 
 # import the necessary packages
-from pyimagesearch.livenessnet import LivenessNet
+from livelinessModel.livenessnet import LivenessNet
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
